3da65ad999738044646037ae7290f334477bf8a4
Prathimakatta <107327259+Prathimakatta@users.noreply.github.com>
2022-06-12 09:13:57 +0100

    Update datatype.py
    Changes from gitHub date 12/06/2022

--- datatype.py
+++ datatype.py
@@ -1,3 +1,4 @@
+//I have changed this file in gitHub 
 Python 3.9.7 (default, Sep 16 2021, 16:59:28) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> sum=0;
